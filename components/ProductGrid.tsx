c2e0136dda7f3863d284dc04e0f2dd09997c23f7
nat254 <natalie.ndetei@strathmore.edu>
2025-03-04 22:06:22 +0300

    feat: added a few tiles and adjusted the filters.

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -58,49 +58,77 @@ const sanitarywareProducts = [
 
 const tilesProducts = [
   {
-    id: 5,
-    name: 'Marble Effect Porcelain Tiles',
-    category: 'porcelain',
-    image: Toilet,
+    id: 1,
+    name: "FGB44689K",
+    category: "floor",
+    subcategory: "30x30",
+    image: "/images/floor1.jpg",
     suggestions: [
-      'Pair with neutral walls',
-      'Add gold accents',
-      'Use warm lighting'
+      "Perfect for shower walls - adds a natural calming vibe",
+      "Pairs well with earthy tones i.e beige, gray, brown",
+      "Pairs well with metallic / wooden / ceramic furniture e.g ceramic sink"
+    ]
+  },
+  {
+    id: 2,
+    name: "FGB44731K",
+    category: "floor",
+    subcategory: "40x40",
+    image: "/images/floo2.jpg",
+    suggestions: [
+      "Perfect for a living room or dining area for a warm, inviting feel",
+      "Pairs well with light-colored walls (off-white or soft taupe)",
+      "Pairs well with black metal accents (like a modern dining table) for contrast."
+    ]
+  },
+  {
+    id: 3,
+    name: "FGB44734K",
+    category: "floor",
+    subcategory: "50x50",
+    image: "/images/floor3.jpg",
+    suggestions: [
+      "Perfect for a kitchen area for an elegant and easy-to-maintain floor",
+      "Pairs well with accent colors like muted greens or blues to add depth.",
+      "Pairs well with minimalist white or gray furniture for a sleek look."
+    ]
+  },
+  {
+    id: 4,
+    name: "FGB44685K",
+    category: "floor",
+    subcategory: "60x60",
+    image: "/images/floor4.jpg",
+    suggestions: [
+      "Perfect for bedroom for a coastal, calming ambiance.",
+      "Pairs well with soft blues, white, and muted grays to enhance the coastal feel",
+      "Pairs well with woven textures like linen or jute for a cozy, organic touch"
+    ]
+  },
+  {
+    id: 5,
+    name: "FGE44754K",
+    category: "floor",
+    subcategory: "40x40",
+    image: "/images/floor5.jpg",
+    suggestions: [
+      "Perfect for kitchens or bathrooms, particularly as a decorative border for countertop edges",
+      "Pairs well with crisp, neutral tones like white, soft gray, or muted metallics",
+      "Pairs well with sleek, modern furniture and minimalist shelving.",
     ]
   },
   {
     id: 6,
-    name: 'Wood Effect Ceramic Tiles',
-    category: 'ceramic',
-    image: Toilet,
+    name: "PCMP24381AK",
+    category: "wall",
+    subcategory: "25x40",
+    image: "/images/wall1.jpg",
     suggestions: [
-      'Mix with natural materials',
-      'Use in herringbone pattern',
-      'Add plants for contrast'
+      "Ideal for flooring or feature walls due to its durable and textured finish.",
+      "Pairs well with warm, earthy tones like terracotta, beige, or sage green to complement its organic or matte appearance.",
+      "Pairs well with natural wood furniture or rustic decor."
     ]
-  },
-  {
-    id: 7,
-    name: 'Modern Mosaic Tiles',
-    category: 'mosaic',
-    image: Toilet,
-    suggestions: [
-      'Create feature wall',
-      'Balance with solid colors',
-      'Add LED strip lighting'
-    ]
-  },
-  {
-    id: 8,
-    name: 'Natural Stone Tiles',
-    category: 'stone',
-    image: Toilet,
-    suggestions: [
-      'Use in wet areas',
-      'Seal for longevity',
-      'Mix sizes for interest'
-    ]
-  },
+  }
 ];
 
 export const ProductGrid = ({ selectedFilters, category }: ProductGridProps) => {
@@ -108,7 +136,8 @@ export const ProductGrid = ({ selectedFilters, category }: ProductGridProps) => 
   
   const filteredProducts = selectedFilters.length === 0
     ? products
-    : products.filter(product => selectedFilters.includes(product.category));
+    : products.filter(product => selectedFilters.includes(product.category) || 
+    ('subcategory' in product && selectedFilters.includes(product.subcategory!)));
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -126,7 +155,11 @@ export const ProductGrid = ({ selectedFilters, category }: ProductGridProps) => 
             <Image
               src={product.image}
               alt={product.name}
-              className="md:w-58 md:h-48 object-cover rounded-sm mb-4"
+              layout='responsive'
+              width={250}
+              height={200}
+              // className="md:w-58 md:h-48 object-cover rounded-sm mb-4"
+              className='object-cover rounded-sm mb-2'
             />
 
             {/* Centered product name */}
@@ -137,8 +170,8 @@ export const ProductGrid = ({ selectedFilters, category }: ProductGridProps) => 
             {/* Suggestions list */}
             <div className="w-full space-y-3">
               {product.suggestions.map((suggestion, index) => (
-                <div key={index} className="flex items-center gap-2">
-                  <div className="w-4 h-4 bg-orange-500 flex-shrink-0 rounded"></div>
+                <div key={index} className="flex items-center md:place-items-start gap-2">
+                  <div className="w-4 h-4 md: mt-1 bg-orange-500 flex-shrink-0 rounded"></div>
                   <p className="text-sm text-gray-600">{suggestion}</p>
                 </div>
               ))}
