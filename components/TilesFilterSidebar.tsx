2845d1a1f90d68fbfe7ed6b78cbfec1a303a8f17
nat254 <natalie.ndetei@strathmore.edu>
2025-03-04 20:15:18 +0300

    feat: add changes to tiles filter

--- components/TilesFilterSidebar.tsx
+++ components/TilesFilterSidebar.tsx
@@ -68,7 +68,7 @@ export const TilesFilterSidebar = ({
     return (
       <aside
         className={cn(
-          "fixed inset-y-0 left-0 transform md:relative md:translate-x-0 z-40",
+          "fixed inset-y-12 left-0 transform md:relative md:translate-x-0 z-40",
           "w-64 bg-white p-6",
           "transition-transform duration-300 ease-in-out",
           isOpen ? "translate-x-0" : "-translate-x-full"
