0a8c9c3dd5ea085537d91ba28a7b3b96c3350208
Kigen29 <emmanuel.kemboi@strathmore.edu>
2025-03-12 12:34:03 +0300

    fix: fix sitemap links

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,10 @@
-export async function GET() {
-    const baseUrl = "https://rangautiles.com";
-  
-    const pages = ["/", "/about", "/sanitaryware" , "/contactus"]; 
-  
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${pages
-        .map(
-          (page) => `<url><loc>${baseUrl}${page}</loc></url>`
-        )
-        .join("")}
-    </urlset>`;
-  
-    return new Response(sitemap, {
-      headers: {
-        "Content-Type": "application/xml",
-      },
-    });
-  }
-  
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    { url: "https://rangautiles.com/", lastModified: new Date() },
+    { url: "https://rangautiles.com/about", lastModified: new Date() },
+    { url: "https://rangautiles.com/sanitaryware", lastModified: new Date() },
+    { url: "https://rangautiles.com/contactus", lastModified: new Date() },
+  ];
+}
